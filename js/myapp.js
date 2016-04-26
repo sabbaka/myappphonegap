3fb3121ec6ed25e2dbae35f8fd2a169c0839e099
Kirill G <kirill@speedflow.com>
2016-04-26 12:13:29 +0300

    Update

---
+++ js/myapp.js
@@ -0,0 +1,22 @@
+angular.module('myApp', [])
+
+angular.module('myApp').controller('mainController', function($scope, $window) {
+    $scope.map = function() {
+        $scope.template = 'map.html';
+    }
+
+    $scope.settings = function() {
+        $scope.template = 'settings.html';
+    }
+
+    $scope.order = function() {
+        $scope.template = 'order.html';
+    }
+
+    $scope.user = function() {
+        $scope.template = 'user.html';
+    }
+
+    $scope.mapWidth = $window.innerWidth;
+    $scope.mapHeight = $window.innerHeight;
+});
